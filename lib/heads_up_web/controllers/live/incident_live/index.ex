32580e13a5bee24d09ba7b862e7a0879fe11bc24
Dr Ed Wallitt <edwallitt@icloud.com>
2024-11-24 11:46:55 +0000

    Update URI params from form

--- lib/heads_up_web/controllers/live/incident_live/index.ex
+++ lib/heads_up_web/controllers/live/incident_live/index.ex
@@ -5,13 +5,17 @@ defmodule HeadsUpWeb.IncidentLive.Index do
   import HeadsUpWeb.CustomComponents
 
   def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  def handle_params(params, _uri, socket) do
     socket =
       socket
       |> assign(:page_title, "Incidents")
-      |> assign(:form, to_form(%{}))
-      |> stream(:incidents, Incidents.list_incidents())
+      |> assign(:form, to_form(params))
+      |> stream(:incidents, Incidents.filter_incidents(params))
 
-    {:ok, socket}
+    {:noreply, socket}
   end
 
   def render(assigns) do
@@ -80,15 +84,20 @@ defmodule HeadsUpWeb.IncidentLive.Index do
           "Priority: Low to High": "priority_asc"
         ]}
       />
+      <.link navigate={~p"/incidents"}>
+        Reset
+      </.link>
     </.form>
     """
   end
 
   def handle_event("filter", params, socket) do
-    socket =
-      socket
-      |> assign(:form, to_form(params))
-      |> stream(:incidents, Incidents.filter_incidents(params), reset: true)
+    params =
+      params
+      |> Map.take(~w(q status sort_by))
+      |> Map.reject(fn {_, v} -> v == "" end)
+
+    socket = push_navigate(socket, to: ~p"/incidents?#{params}")
 
     {:noreply, socket}
   end
